0e8a1471e1fc49e694e007c4c870666396d7b195
Michael DeSanty <michael.desanty@gmail.com>
2023-03-04 17:33:28 -0500

    Recursion Assignment Solution

---
+++ src/assignments/recursion/Main.java
@@ -0,0 +1,14 @@
+package assignments.recursion;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Recursion.removeX("HaxxedHeXX"));
+        System.out.println(Recursion.removeX("Already no letter"));
+
+        System.out.println(Recursion.sumDigits(1403));
+        System.out.println(Recursion.sumDigits(4));
+
+        System.out.println(Recursion.countSubstring("catwoman loves cats", "cat"));
+        System.out.println(Recursion.countSubstring("xxxx", "xxx"));
+    }
+}
